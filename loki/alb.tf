00cc6304e9f3e0cebcd2f674cb893587ed5e3c2b
Aleksandr Dudkin <aleks@dudkin.cz>
2026-06-07 16:22:15 +0200

    Add explanatory comments to ALB

--- loki/alb.tf
+++ loki/alb.tf
@@ -1,7 +1,6 @@
 # internet :80  ->  aws_lb.loki-alb  ->  aws_lb_listener (:80, forward)
 # -> aws_lb_target_group.loki-nginx-gateway-target-group (:8080)  ->  [nginx gateway tasks]
 
-
 resource "aws_lb" "loki-alb" {
   name               = "loki-alb"
   internal           = false         # Public DNS name and IP
@@ -9,6 +8,10 @@ resource "aws_lb" "loki-alb" {
   security_groups    = [aws_security_group.loki-alb.id]
   subnets            = data.aws_subnets.default.ids # By default has a list of 3 AZ subnets
 }
+
+# Listener — "what port do I accept on?"
+# Listener rule — "if the request looks like X, send it to Y instead."
+# Target group - "who to send to + health check rule"
 
 # Makes the ALB listen on a port and decides what to do with arriving requests
 resource "aws_lb_listener" "loki-alb-listener" {
@@ -41,6 +44,7 @@ resource "aws_lb_listener" "grafana-https" {
   }
 }
 
+# Expose Loki API for log gathering
 resource "aws_lb_listener_rule" "loki-api" {
   listener_arn = aws_lb_listener.loki-alb-listener.arn
   priority     = 100 # The lower the number the higher the priority is
